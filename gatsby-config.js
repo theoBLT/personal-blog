2164607ec74a81edce560b2429e5fc05c10e2974
[theoBLT] <[theobloc@gmail.com]>
2020-10-24 18:15:12 +0100

    [feat] Add basic google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        head:true,
       },
     },
     `gatsby-plugin-sitemap`,
